f2d63c826d4d5d2611e378788177c73baadd7833
orcunpolatoglu <115425870+orcunpolatoglu@users.noreply.github.com>
2022-10-26 14:50:35 +0300

    Add files via upload

--- Flatten.py
+++ Flatten.py
@@ -1,3 +1,4 @@
+
 def flatten(x):
     
     for i in x:
@@ -5,10 +6,35 @@ def flatten(x):
             flatten(i)
         else:
             emp_list.append(i)
-    
-    
+    #print(emp_list)
+
+
+def reverse_list(x):
+     
+    for i in range(int(len(x)/2)):
+        temp = x[i]
+        x[i] = x[len(x)-(i+1)]
+        x[len(x)-(i+1)] = temp
+        
+    counter = 0
+    for i in x:
+        if isinstance(i,list):
+             
+            for a in range(int(len(i)/2)):
+                temp = x[counter][a]
+                x[counter][a] = x[counter][len(i)-(a+1)]
+                x[counter][len(i)-(a+1)] = temp
+                counter +=1
+    print(x)   
 
 x= [[1,'a',['cat'],2],[[[3]],'dog'],4,5] # Verilen Liste
-emp_list = [] 
+y = [[1, 2], [3, 4], [5, 6, 7]] # Verilen Liste
+emp_list = []
 flatten(x)
 print(emp_list)
+
+reverse_list(y)
+
+
+
+
